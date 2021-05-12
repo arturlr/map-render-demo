b866ca62959a7764e33959b0606d8b9680415421
Rodrigues <artrodri@a483e77799ff.ant.amazon.com>
2021-05-11 23:08:22 -0700

    deployment update

--- test.sh
+++ test.sh
@@ -6,19 +6,15 @@ fi
 
 DEVICE=$1
 
+PROJECT_NAME=$(cat ./amplify/.config/project-config.json | jq -r '.projectName')
+
+
 function updateTracker() {
     DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
     echo "Updating Device $DEVICE at LGN $1 LAT $2 at $DATE"
-    aws location batch-update-device-position --tracker-name appTracker --updates DeviceId=$DEVICE,Position=$1,$2,SampleTime=$DATE --no-cli-pager
+    aws location batch-update-device-position --tracker-name $PROJECT_NAME.tracker --updates DeviceId=$DEVICE,Position=$1,$2,SampleTime=$DATE --no-cli-pager
     sleep 15
 }
-
-# Vancouver
-# updateTracker -123.1268574 49.2632545
-# updateTracker -123.1254942 49.2627456
-# updateTracker -123.1228227 49.261643
-# updateTracker -123.1219242 49.262114
-# updateTracker -123.1216613 49.2622426
 
 # Central Park
 updateTracker -73.9687025 40.7812239
